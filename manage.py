4a6c1b5711bd568220f8666707087bd46535f4ef
Mario Arturo Arias Chaves <ariaschmario@gmail.com>
2020-09-24 00:01:02 -0600

    trying to debug

--- manage.py
+++ manage.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 import os
 import sys
+from djecommerce.wsgi import application
+
+# App Engine by default looks for a main.py file at the root of the app
+# directory with a WSGI-compatible object called app.
+# This file imports the WSGI-compatible object of your Django app,
+# application from mysite/wsgi.py and renames it app so it is discoverable by
+# App Engine without additional configuration.
+# Alternatively, you can add a custom entrypoint field in your app.yaml:
+# entrypoint: gunicorn -b :$PORT mysite.wsgi
+breakpoint_enable_canary = False
+app = application
+
 breakpoint_enable_canary = False
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings.development')
@@ -13,3 +25,5 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
