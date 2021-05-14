6fd4d6a02a3d909b94ac900a5ab81437ca7bb73b
ariaschmario <ariaschmario@gmail.com>
2021-05-13 19:08:07 -0600

    - Deleting PaymentView.py

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -2,7 +2,6 @@ from .CheckoutView import CheckoutView
 from .HomeView import HomeView
 from .ItemDetailView import ItemDetailView
 from .OrderSummaryView import OrderSummaryView, Get_Order
-from .PaymentView import PaymentView
 from .RequestRefundView import RequestRefundView
 from .AddCouponView import AddCouponView
 from .CatalogView import CatalogView
