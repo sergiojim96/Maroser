879900c37487c2b0f6122418227f8288d5e01154
sergio <sergioajm2909@gmail.com>
2021-06-03 01:05:46 +0000

    setting mail in production

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -97,9 +97,9 @@ LOGGING = {
 }
 
 # gmail_send/settings.py
+EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sashashopcr@gmail.com'
 EMAIL_HOST_PASSWORD = 'littasabzshwesgo'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
